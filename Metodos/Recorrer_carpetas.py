5ffb61386573918aa4dd7c58662be508eebb92de
Didier Alejandro Martinez Molina <didier1997@hotmail.es>
2020-03-24 13:54:09 -0500

    Control de execciones y remover archivo anterior

--- Metodos/Recorrer_carpetas.py
+++ Metodos/Recorrer_carpetas.py
@@ -4,7 +4,7 @@ from os import listdir
 from os.path import isfile, isdir, join
 import xml.etree.ElementTree as et
 import shutil
-
+from os import remove
 
 def listdir_recurd(files_list, root, d, checked_folders):
     pass
@@ -32,18 +32,28 @@ def Leer (texto):
     return url.replace("\\","/").replace("‪","")
 
 #Retorna una matriz con todos las etiquetas de un xml "Name"
-def RecorrerXML(url,destino,etiquetas,archivos):
-    matrizXML=et.parse(url)
-    root= matrizXML.getroot()
-    for hijo in root.findall('object'):
-       for nieto in hijo.findall('name'):
-            if ValidarEtiquetas(nieto.text,etiquetas)==0:
-                urlTemp=url.replace(archivos,destino)
-                MoverArchivo(url,urlTemp)
-                urlActImg=url.replace(".xml",".jpg")
-                urlTempImg=urlActImg.replace(archivos,destino)
-                MoverArchivo(urlActImg, urlTempImg)
-
+def RecorrerXML(url,destino,etiquetas,archivos,rutaMalos):
+  try:
+      matrizXML = et.parse(url)
+      root = matrizXML.getroot()
+      for hijo in root.findall('object'):
+          for nieto in hijo.findall('name'):
+              if ValidarEtiquetas(nieto.text, etiquetas) == 0:
+                  urlTemp = url.replace(archivos, destino)
+                  MoverArchivo(url, urlTemp)
+                  urlActImg = url.replace(".xml", ".jpg")
+                  urlTempImg = urlActImg.replace(archivos, destino)
+                  MoverArchivo(urlActImg, urlTempImg)
+                  remove(url)
+                  remove(urlActImg)
+  except:
+      urlMalosNew=url.replace(archivos,rutaMalos)
+      MoverArchivo(url, urlMalosNew)
+      urlMalosImg=url.replace(".xml", ".jpg")
+      urlMalosNewImg=urlMalosImg.replace(archivos,rutaMalos)
+      MoverArchivo(urlMalosImg,urlMalosNewImg)
+      remove(urlMalosImg)
+      remove(url)
 
 #Compara la etiqueta enviada con la del archivo txt
 def ValidarEtiquetas(etiqueta,etiquetas):
@@ -62,13 +72,14 @@ def Main():
     print("recuerde que las rutas de los archivos deben ser completas terminando por / o \\: \n")
     rutaArchivos=Leer("Ingrese la Direccion del directorio de los archivos: ").strip()
     rutaDestino=Leer("Ingrese la direccion del destino para almacenar defectuosos: ").strip()
-    rutaEtiquetas=Leer("Ingrese la direcciond del txt que ocntiene las etiquetas: ").strip()
+    rutaEtiquetas=Leer("Ingrese la direcciond del txt que contiene las etiquetas: ").strip()
+    rutaMalos = Leer("Ingrese la direccion Para almacenar malos: ").strip()
 
     matriz=RecorrerDirectorio([],rutaArchivos,rutaArchivos,[])
 
     for ruta in matriz:
         if ".xml" in ruta:
-            RecorrerXML(ruta,rutaDestino,rutaEtiquetas,rutaArchivos)
+            RecorrerXML(ruta,rutaDestino,rutaEtiquetas,rutaArchivos,rutaMalos)
 
 
 if __name__ == "__main__":
